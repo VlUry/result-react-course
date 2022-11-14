b5a5b582407b5f7c8188a954efd663d50348c52a
Vladislav Urykov <vladislavurykov29@gmail.com>
2022-11-14 09:09:12 +0300

    dz-1

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,5 @@
 const Counter = (props) => {
-  const { id, value, name, onDelete } = props;
+  const { id, value, name, onDelete, onIncrement, onDecrement } = props;
 
   const formatCount = () => {
     return value === 0 ? "empty" : value;
@@ -11,8 +11,20 @@ const Counter = (props) => {
     <div>
       <span>{name}</span>
       <span className={`${bgColor} badge m-2`}>{formatCount()}</span>
-      <button className="btn btn-primary btn-sm m-2">+</button>
-      <button className="btn btn-primary btn-sm m-2">-</button>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => onIncrement(id)}
+      >
+        +
+      </button>
+      <button
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => {
+          if (value > 0) onDecrement(id);
+        }}
+      >
+        -
+      </button>
       <button
         className="btn btn-danger btn-sm m-2"
         onClick={() => onDelete(id)}
